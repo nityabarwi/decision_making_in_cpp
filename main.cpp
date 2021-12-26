d7f0bec3fd1bc16dca043b172737556ea1fdf328
nityabarwi <95531972+nityabarwi@users.noreply.github.com>
2021-12-26 02:09:22 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l,b;
+    cout<<"enter length and breadth"<<endl;
+    cin>>l>>b;
+    if(l==b)
+    
+        cout<<"the rectangle is a square"<<endl;
+    
+    else
+        cout<<"the rectangle is not a square"<<endl;
+return 0;        
+}
